e0e17716bcb87683a5092db36afe7de8fa7da768
Vimal Madhavan <vimal.madhavan@intigral.net>
2017-05-03 15:42:59 +0400

    Fixed comment add feature

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -26,7 +26,7 @@ module.exports = WelcomeCtrl;
 },{}],2:[function(require,module,exports){
 (function(){
 'use strict';
-var commentWidget = function($parse, $rootScope, $timeout) {
+var commentWidget = function($parse, $rootScope, $timeout, $compile) {
 	return {
 		restrict: 'E',
 		templateUrl:"views/comment.html",
@@ -34,22 +34,41 @@ var commentWidget = function($parse, $rootScope, $timeout) {
 		transclude:false,
 		controller:function($scope){
 			$scope.commentContent = "";
+			$scope.deleteText = "delete";
+			$scope.editText = "edit";
 			$scope.header = "Add Comment";
 			$scope.nameHeader = "Your name";
 			$scope.commentHeader="Your comment";
 			$scope.commentLengthLimit = 500;
+			$scope.comments = [];
 			$scope.limitReached = function(){
 				return $scope.commentContent.length >= $scope.commentLengthLimit;
 			};
-			$scope.save = function(){
-				angular.element(document.querySelector('#'+$scope.post_id)).append('<div><div>'+$scope.commentName+'</div><div>'+$scope.commentContent+'</div></div>');
+			$scope.delete = function(index){
+				$scope.comments.splice(index, 1);
+			}
+			$scope.save = function(index){
+				if(angular.isDefined(index)){
+					$scope.comments[index] = {commentName : $scope.commentName, commentContent : $scope.commentContent};
+				} else {
+					$scope.comments.push({commentName : $scope.commentName, commentContent : $scope.commentContent});
+				}
 				$scope.commentWidgetIsActive = false;
 			};
+			$scope.edit = function(index){
+				$scope.index = index;
+				$scope.commentName = $scope.comments[index]['commentName'];
+				$scope.commentContent = $scope.comments[index]['commentContent'];
+				$scope.commentWidgetIsActive = true;
+			}
 
 		},
 		link: function(scope,ele, attrs){
 			scope.post_id = attrs['id'];
-			console.log('commentWidget in Link');
+			var commentTemplate = '<div class="comments"><div ng-repeat="comment in comments" class="comment-{{$index}}"><div class="name"> {{comment.commentName }} </div><div class="content"> {{ comment.commentContent }} </div><div class="delete" ng-click="delete($index)">{{ deleteText }}</div><div class="edit" ng-click="edit($index)"> {{ editText }} </div></div></div>';
+			var content = $compile(commentTemplate)(scope);
+			angular.element(document.querySelector('#'+scope.post_id)).append(content);
+
 			scope.$on('OPEN_COMMENT_WIDGET', function(evt, args){
 				if(args.post_id === scope.post_id){
 					scope.commentWidgetIsActive = true;
